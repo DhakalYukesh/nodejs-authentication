c7419f7a0697cea303d56a053f194af8d7d7a267
Yukesh Dhakal <yukeshdhakal42@gmail.com>
2024-03-13 15:19:14 +0545

    add routes and crud controllers

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 // Imports
 const express = require('express');
 const errorHandler = require('./middleware/errorhandler');
+const connectDb = require('./config/dbConnection');
 const app = express();
 require('dotenv').config();
+
+// Connections
+connectDb();
+const PORT = process.env.PORT || 5000;
 
 // Routes and Middleware
 app.use(express.json()); // Parse JSON
@@ -10,7 +15,6 @@ app.use('/api/contacts', require('./routes/contactRoutes'));
 app.use(errorHandler);
 
 // Server
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
